07a18b7cd81f513a779d057d230434c99e2f934e
epifab <epifab@gmail.com>
2019-10-30 09:09:15 +0000

    Added missing BinaryExpr operators for placeholders as string

--- src/main/scala/io/epifab/yadl/typesafe/Implicits.scala
+++ src/main/scala/io/epifab/yadl/typesafe/Implicits.scala
@@ -8,16 +8,6 @@ object Implicits {
   }
 
   implicit class ExtendedField[F1 <: Field[_]](field1: F1) {
-    def ===[NAME <: String with Singleton, T]
-        (placeholderName: NAME)
-        (implicit
-         valueOf: ValueOf[NAME],
-         fieldT: FieldT[F1, T],
-         fieldDecoder: FieldDecoder[T],
-         fieldEncoder: FieldEncoder[T],
-         areComparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): Equals[F1, Placeholder[T, T] with Tag[NAME]] =
-      Equals(field1, Placeholder[T, NAME])
-
     def ===[F2 <: Field[_]](field2: F2)(implicit comparable: AreComparable[F1, F2]): Equals[F1, F2] =
       Equals(field1, field2)
 
@@ -47,5 +37,104 @@ object Implicits {
 
     def in[F2 <: Field[_]](field2: F2)(implicit canBeIncluded: CanBeIncluded[F1, F2]): IsIncluded[F1, F2] =
       IsIncluded(field1, field2)
+
+    def ===[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         comparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): Equals[F1, Placeholder[T, T] with Tag[NAME]] =
+      Equals(field1, Placeholder[T, NAME])
+
+    def !==[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         comparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): NotEquals[F1, Placeholder[T, T] with Tag[NAME]] =
+      NotEquals(field1, Placeholder[T, NAME])
+
+    def <[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         comparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): LessThan[F1, Placeholder[T, T] with Tag[NAME]] =
+      LessThan(field1, Placeholder[T, NAME])
+
+    def >[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         comparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): GreaterThan[F1, Placeholder[T, T] with Tag[NAME]] =
+      GreaterThan(field1, Placeholder[T, NAME])
+
+    def <=[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         comparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): LessThanOrEqual[F1, Placeholder[T, T] with Tag[NAME]] =
+      LessThanOrEqual(field1, Placeholder[T, NAME])
+
+    def >=[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         comparable: AreComparable[F1, Placeholder[T, T] with Tag[NAME]]): GreaterThanOrEqual[F1, Placeholder[T, T] with Tag[NAME]] = GreaterThanOrEqual(field1, Placeholder[T, NAME])
+
+    def subsetOf[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         canBeSubset: CanBeSubset[F1, Placeholder[T, T] with Tag[NAME]]): IsSubset[F1, Placeholder[T, T] with Tag[NAME]] =
+      IsSubset(field1, Placeholder[T, NAME])
+
+    def supersetOf[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         canBeSuperset: CanBeSuperset[F1, Placeholder[T, T] with Tag[NAME]]): IsSuperset[F1, Placeholder[T, T] with Tag[NAME]] =
+      IsSuperset(field1, Placeholder[T, NAME])
+
+    def overlaps[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[T],
+         fieldDecoder: FieldDecoder[T],
+         canOverlap: CanOverlap[F1, Placeholder[T, T] with Tag[NAME]]): Overlaps[F1, Placeholder[T, T] with Tag[NAME]] =
+      Overlaps(field1, Placeholder[T, NAME])
+
+    def in[NAME <: String with Singleton, T]
+        (placeholderName: NAME)
+        (implicit
+         valueOf: ValueOf[NAME],
+         fieldT: FieldT[F1, T],
+         fieldEncoder: FieldEncoder[Seq[T]],
+         fieldDecoder: FieldDecoder[Seq[T]],
+         canBeIncluded: CanBeIncluded[F1, Placeholder[Seq[T], Seq[T]] with Tag[NAME]]): IsIncluded[F1, Placeholder[Seq[T], Seq[T]] with Tag[NAME]] =
+      IsIncluded(field1, Placeholder[Seq[T], NAME])
   }
 }
